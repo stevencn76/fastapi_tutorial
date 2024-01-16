9e9a47aa20a6f0190b3cf03aeb37a956e5be9736
Steven Chen <stevencn76@gmail.com>
2024-01-16 01:22:22 -0800

    7. example data

--- request_body_advanced/main.py
+++ request_body_advanced/main.py
@@ -10,8 +10,17 @@ app = FastAPI()
 
 
 class Address(BaseModel):
-    address: str
-    postcode: str
+    address: str = Field(..., examples=["5 Queens Street"])
+    postcode: str = Field(..., examples=["0765"])
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [{
+                "address": "2 Queens Street",
+                "postcode": "0987"
+            }]
+        }
+    }
 
 
 class User(BaseModel):
@@ -30,8 +39,15 @@ class Item(BaseModel):
     features: List[Feature]
 
 
+def add(*, num1: int, num2: int):
+    return num1 + num2
+
+
+print(add(num1=1, num2=2))
+
+
 @app.put('/carts/{cart_id}')
-async def update_cart(cart_id: int, user: User, item: Item, count: int = Body(..., ge=2)):
+async def update_cart(*, cart_id: int, user: User = Body(..., examples=[{"username": "Smith"}]), item: Item, count: int = Body(..., ge=2, examples=[8])):
     print(user.username)
     print(item.name)
     result_dict = {
